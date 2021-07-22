97c058b34c8bc8202d4c4867ddf6a75d95d0a474
Anubhab <bhubhik@gmail.com>
2021-07-22 22:19:29 +1000

    Image updated

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,14 +1,14 @@
-
 import classes from './MeetupDetail.module.css';
+import Image from 'next/image';
 
-const MeetupDetail = (props) =>{
-  return(
+const MeetupDetail = (props) => {
+  return (
     <section className={classes.content}>
-      <img src={props.image} alt={props.title} />
+      <Image src={props.image} alt={props.title} />
       <h1>{props.title}</h1>
       <p>{props.description}</p>
       <h3>{props.address}</h3>
     </section>
-  )
-}
-  export default MeetupDetail;
+  );
+};
+export default MeetupDetail;
--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,19 +1,20 @@
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
-import {useRouter} from 'next/router'
+import { useRouter } from 'next/router';
+import Image from 'next/image';
 
 function MeetupItem(props) {
-   const router = useRouter();
+  const router = useRouter();
 
-  const detailsHandler = () =>{
-    router.push('/'+ props.id)
-  }
+  const detailsHandler = () => {
+    router.push('/' + props.id);
+  };
 
   return (
     <li className={classes.item}>
       <Card>
         <div className={classes.image}>
-          <img src={props.image} alt={props.title} />
+          <Image src={props.image} alt={props.title} />
         </div>
         <div className={classes.content}>
           <h3>{props.title}</h3>
